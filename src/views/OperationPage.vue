07532ce1e6c3c827146be24c320deb1674622243
JoseMariaConcha <jomconcal@gmail.com>
2026-04-28 21:35:16 +0200

    dropdown

--- src/views/OperationPage.vue
+++ src/views/OperationPage.vue
@@ -2,6 +2,7 @@
 
 import {useOperations} from "@/stores/OperationsStore.ts";
 import {computed} from "vue";
+import SelectAssetInput from "@/components/common/SelectAssetInput.vue";
 
 const props = defineProps<{
   type: string
@@ -11,7 +12,26 @@ const operation = computed(() => useOperations().findByType(props.type))
 </script>
 
 <template>
-  <h1>{{ operation.type }}</h1>
+  <div class="flex flex-col">
+
+    <h1 class="w-full text-2xl font-semibold text-gray-800 py-6 text-center">
+      {{ operation.type }}
+    </h1>
+
+    <div class="flex flex-col lg:flex-row gap-6">
+
+      <div class="
+        overflow-x-auto overflow-y-visible px-2 lg:px-3
+        flex flex-row gap-3 flex-nowrap scrollbar-hidden
+        lg:flex-col lg:overflow-visible "
+      >
+        <SelectAssetInput/>
+        <SelectAssetInput/>
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
