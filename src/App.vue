0a035233a3c06ac0a310c0d22a1d38ce2d2a2191
JoseMariaConcha <jomconcal@gmail.com>
2026-05-02 01:10:51 +0200

    overflow and bg solved

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@ import NavigationView from "@/views/NavigationView.vue";
 </script>
 
 <template>
-  <div class="h-screen flex flex-col bg-gray-50">
+  <div class="h-screen flex flex-col">
     <nav class="h-[10%] bg-gray-800">
       <div class="w-93.75 mx-auto md:w-300">
         <NavigationView/>
       </div>
     </nav>
 
-    <main class="h-[90%] overflow-y-auto">
+    <main class="h-[90%] overflow-y-scroll">
       <div class="w-93.75 mx-auto my-2 px-2
       md:px-4 md:w-300 md:my-6">
         <router-view />
