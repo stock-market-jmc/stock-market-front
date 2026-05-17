d8c3d6b25ac6bb08e275b24933633f51567777c6
JoseMariaConcha <jomconcal@gmail.com>
2026-05-17 19:46:05 +0200

    add more home options

--- src/stores/FinnHub/SelectTickersStore.ts
+++ src/stores/FinnHub/SelectTickersStore.ts
@@ -5,7 +5,7 @@ import type TickerInterface from "@/types/TickerInterface.ts";
 import type {SelectOption} from "@/types/SelectOption.ts";
 
 const EMPTY_TICKER = {symbol: '', companyName: 'Select a company'} as TickerInterface
-const EMPTY_SELECT_OPTION = {label: 'Select an company', value: EMPTY_TICKER, selected: false} as SelectOption<TickerInterface>
+const EMPTY_SELECT_OPTION = {label: 'Select a company', value: EMPTY_TICKER, selected: false} as SelectOption<TickerInterface>
 
 
 export const useSelectTickersStore = defineStore("selectTickersStore", () => {
--- src/stores/HomeOptionsStore.ts
+++ src/stores/HomeOptionsStore.ts
@@ -1,9 +1,12 @@
 import {defineStore} from 'pinia'
 import type {HomeOptionInterface} from "@/types/HomeOptionInterface.ts";
+
 export const useHomeOptionsStore = defineStore('homeOptionsStore', () => {
 
     const homeOptions: HomeOptionInterface[]= [
         {slug: 'operations', name: 'Operations', description: 'Find different operations in FinnHub API'},
+        {slug: 'investments', name: 'Investments', description: 'Track your earnings and losses'},
+        {slug: 'profile', name: 'Profile', description: 'Manage your personal information and account settings'}
     ]
 
     return {
