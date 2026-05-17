71bb53ae9c490dbfa775dbb9a0e7843476c7dd85
JoseMariaConcha <jomconcal@gmail.com>
2026-05-17 17:03:48 +0200

    login

--- src/views/NavigationView.vue
+++ src/views/NavigationView.vue
@@ -20,52 +20,22 @@ const logout = () => {
 </script>
 
 <template>
-
   <nav class="fixed top-0 left-0 w-full bg-gray-800 z-10 h-16">
     <div class="md:w-300 flex items-center justify-between m-auto h-full px-4">
-      <router-link to="/" class="text-white hover:text-gray-300 transition-colors">
-        <ButtonGeneric
-            title="Home"
-            class="px-6 hidden md:block"
-        />
-        <HomeIcon class="md:hidden inline-flex items-center"/>
+      <router-link to="/" class="text-white hover:text-gray-300 transition-colors p-1">
+        <HomeIcon />
       </router-link>
 
       <h1 class="text-white text-3xl m-0">{{ title }}</h1>
 
-      <div v-if="!isLoggedIn" class="hidden md:block">
-        <router-link to="/login">
-          <ButtonGeneric
-              title="Login"
-              class="px-6"
-          />
+      <div class="flex items-center">
+        <router-link v-if="!isLoggedIn" to="/login" class="text-white hover:text-gray-300 transition-colors p-1">
+          <LoginIcon />
         </router-link>
-      </div>
-      <div v-else class="hidden md:block">
-        <ButtonGeneric
-            title="Logout"
-            class="px-6"
-            @clicked="logout"
-        />
-      </div>
-
-      <div
-          v-if="!isLoggedIn"
-          class="md:hidden inline-flex items-center">
-        <router-link to="/login">
-          <LoginIcon/>
-        </router-link>
-      </div>
-      <div v-else
-           class="md:hidden inline-flex items-center">
-         <LogoutIcon
-            v-on:click="logout"
-        />
+        <span v-else @click="logout" class="text-white hover:text-gray-300 transition-colors cursor-pointer p-1">
+          <LogoutIcon />
+        </span>
       </div>
     </div>
-
   </nav>
 </template>
-<style scoped>
-
-</style>
